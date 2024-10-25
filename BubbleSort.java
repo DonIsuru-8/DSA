974bc90a12e00c25e9618e8b9c025027ee848fb2
DonIsuru-8 <isuru.samarakkody0808@gmail.com>
2024-10-25 22:34:03 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+	public class BubbleSort {
+    public static void main(String[] args) {
+
+	   int i=1;
+	   int j=6;
+	   System.out.println("before swapping");
+	   System.out.println( "i: " +i);
+	   System.out.println( "j: " +j);
+	   
+	   int temp=i;
+	   i=j;
+	   j=temp;
+	   
+	   System.out.println("after swapping");
+	   System.out.println( "i: " +i);
+	   System.out.println( "j: " +j);
+	   
+	   }
+	}
